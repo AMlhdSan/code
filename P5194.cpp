c7d5f56b5e865f3d69ac1305f2e240b3cb224109
AMlhd <amlhdsan@gmail.com>
2024-09-11 22:17:04 +0800

    upload

--- P5194.cpp
+++ P5194.cpp
@@ -10,9 +10,14 @@ int main() {
     ios::sync_with_stdio(false);
 
     cin >> n >> c;
+    a[0] = 0;
 
     for(int i = 1; i <= n; ++i) {
         cin >> a[i];
+        a[i] += a[i - 1];
+        if(a[i] > c) {
+
+        }
     }
 
     cout << maxx << endl;
