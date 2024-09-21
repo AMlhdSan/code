710fd08ff8af1d4afb3d6d8d87a85ce70a7b26c3
AMlhd <amlhdsan@gmail.com>
2024-09-21 22:32:40 +0800

    upload

---
+++ 线段树.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int a[100001];
+int op;
+int x, y, k;
+
+int main() {
+
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
+
+    while(m--) {
+        cin >> op;
+        if(op == 1) {
+            cin >> x >> y >> k;
+        }
+        else {
+
+        }
+    }
+    return 0;
+}
