05cb9e4ea1e74884c2591f642b0385dae0711322
AMlhd <amlhdsan@gmail.com>
2024-09-21 19:18:46 +0800

    upload

---
+++ kruskal_my.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, k;
+int f[200001];
+int ans = 0;
+int cnt = 0;
+
+struct Edge {
+    int u, v, c;
+};
+
+Edge edges[200001];
+
+bool cmp(Edge a, Edge b) {
+    return a.c < b.c;
+}
+
+void add_edge(int uu, int vv, int cc, int index) {
+    edges[index].c = cc;
+    edges[index].u = uu;
+    edges[index].v = vv;
+}
+
+int find_root(int x) {
+    if(f[x] == x)
+        return x;
+    else
+        return f[x] = find_root(f[x]);
+}
+
+void merge(int x, int y) {
+    f[find_root(x)] = find_root(y);
+}
+
+
+
+void kruskal() {
+    for(int i = 1; i <= m; ++i) {
+        int fu = find_root(edges[i].u);
+        int fv = find_root(edges[i].v);
+        if(fu != fv) {
+            merge(fu, fv);
+            ans += edges[i].c;
+            cnt++;
+        }
+        if(cnt == n - 1) {
+            cout << ans << endl;
+            return;
+        }
+    }
+    cout << "orz" << endl;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; ++i) 
+        f[i] = i;
+
+    for(int i = 1; i <= m; ++i) {
+        int uu, vv, cc;
+        cin >> uu >> vv >> cc;
+        add_edge(uu, vv, cc, i);
+    }
+
+    sort(edges + 1, edges + m + 1, cmp);
+
+    kruskal();
+    return 0;
+}
