92f183c271986622cbacbdd93b810dc2f3f3be21
AMlhd <amlhdsan@gmail.com>
2024-09-22 22:58:09 +0800

    upload

---
+++ P4588.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define N 100001
+#define ll long long
+#define ls (p << 1)
+#define rs (p << 1 | 1)
+using namespace std;
+
+int T;
+int q, mod;
+ll tree[N << 1];
+
+void build(int p, int l, int r) {
+
+}
+
+int main() {
+
+    cin >> T;
+    while(T--) {
+        cin >> q >> mod;
+
+        build(1, 1, q);
+
+        while(q--) {
+
+        }
+    }
+
+    return 0;
+}
