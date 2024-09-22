7681746ec63da011df6a7824ad2864cbef119213
AMlhd <amlhdsan@gmail.com>
2024-09-22 23:12:45 +0800

    upload

--- P4588.cpp
+++ P4588.cpp
@@ -7,10 +7,35 @@ using namespace std;
 
 int T;
 int q, mod;
-ll tree[N << 1];
+ll tree[N << 2];
+int op, t;
+
+void upd(int p) {
+    tree[p] = (tree[ls] * tree[rs]) % mod;
+}
 
 void build(int p, int l, int r) {
+    if(l == r) {
+        tree[p] = 1;
+        return;
+    }
+    int mid = (l + r) >> 1;
+    build(ls, l, mid);
+    build(rs, mid + 1, r);
+    upd(p);
+}
 
+void revice(int p, int l, int r, int ql, int qr, int k) {
+    if(ql <= l && r <= qr) {
+        tree[p] = k;
+        return;
+    }
+    int mid = (l + r) >> 1;
+    if(ql <= mid)
+        revice(ls, l, mid, ql, qr, k);
+    if(qr > mid)
+        revice(rs, mid + 1, r, ql, qr, k);
+    upd(p);
 }
 
 int main() {
@@ -21,8 +46,15 @@ int main() {
 
         build(1, 1, q);
 
-        while(q--) {
-
+        for(int i = 1; i <= q; ++i) {
+            cin >> op >> t;
+            if(op == 1) {
+                revice(1, 1, q, i, i, t);
+            }
+            else
+                revice(1, 1, q, t, t, 1);
+            
+            cout << tree[1] % mod << endl;
         }
     }
 
