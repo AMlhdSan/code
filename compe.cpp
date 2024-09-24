fd8f7743ed4986c6c46abfc9423c5d08df6742d4
AMlhd <amlhdsan@gmail.com>
2024-09-24 20:49:40 +0800

    upload

---
+++ compe.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[1001][9];
+struct node {
+    double average, xx;
+    int sum;
+};
+
+node x[1001];
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i][1] >> a[i][2] >> a[i][3] >> a[i][4] >> a[i][5] >> a[i][6] >> a[i][7] >> a[i][8];
+        s[i].average = (a[i][1] + a[i][2] + a[i][3] + a[i][4] + a[i][5] + a[i][6] + a[i][7] + a[i][8]) / 8;
+        s[i].sum = a[i][1] + a[i][2] + a[i][3] + a[i][4] + a[i][5] + a[i][6] + a[i][7] + a[i][8];
+        double t = 0;
+        for(int j = 1; j <= 8; ++j) {
+
+        }
+        for(int j = 1; j <= 8; ++j) {
+
+        }
+    }
+    
+    return 0;
+}
