7f8a6dc9b9479a3a5b242738279c5c36e4b6cd41
AMlhd <amlhdsan@gmail.com>
2024-10-05 23:37:47 +0800

    upload

--- P1880.cpp
+++ P1880.cpp
@@ -6,11 +6,37 @@ using namespace std;
 
 int dp[N << 1][N << 1];
 int a[N];
+int f[N << 1];
 int n;
+int sum[N << 1];
 
 int main() {
 
+    cin >> n;
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        f[i] = f[i + n] = a[i];
+        sum[i + n] = sum[i] = sum[i - 1] + a[i];
+    }
 
+    for(int l = 2; l <= n; ++l) {
+        for(int i = 1; i <= (n << 1) - l + 1; ++i) {
+            int j = i + l - 1;
+            for(int k = i; k <= j - 1; ++k) {
+                dp[i][j] = max(dp[i][j], dp[i][k] + dp[k + 1][j] + sum[j] - sum[i - 1]);
+            }
+        }
+    }
+
+    int maxx = -0x7fffffff;
+
+    for(int i = 1; i <= n; ++i) {
+        maxx = max(maxx, dp[i][i + n - 1]);
+    }
+
+    cout << endl;
+
+    cout << maxx << endl;
 
     return 0;
 }
