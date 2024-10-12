9dace694bc37f76a9ce875c4085c13a962c64831
AMlhdSan <amlhdsan@gmail.com>
2024-10-12 21:19:48 +0800

    upload

--- P2340.cpp
+++ P2340.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int n;
 int s[N], f[N];
 int dp[N][N];
+int ss[N], ff[N];
 
 int main() {
 
@@ -15,6 +16,12 @@ int main() {
     for(int i = 1; i <= n; ++i) 
         cin >> s[i] >> f[i];
     
-    
+    dp[0][0] = 0;
+
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= n; ++j) {
+            dp[i][j] = ;
+        }
+    }
     return 0;
 }
