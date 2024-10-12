9dd908317e30d202f0e94c2e38ed891de4c70426
AMlhdSan <amlhdsan@gmail.com>
2024-10-12 21:48:44 +0800

    upload

--- CF607B.cpp
+++ CF607B.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
+
+#define N 501
+
 using namespace std;
 
-int 
+int dp[N][N];
+int a[N];
+int n;
 
 int main() {
+
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
+
+    for(int i = 1; i <= n - 1; ++i) {
+        if(a[i] == a[i + 1]) {
+            dp[i][i + 1] = 1;
+        }
+        else {
+            dp[i][i + 1] = 2;
+        }
+    }
 
     return 0;
 }
