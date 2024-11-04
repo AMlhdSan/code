d6360e04f04b97e5fe36b29fce92e59f2df3b2a6
AMlhdSan <amlhdsan@gmail.com>
2024-11-04 23:25:01 +0800

    upd

--- Luogu_P_1926.cpp
+++ Luogu_P_1926.cpp
@@ -1,15 +1,60 @@
 #include <bits/stdc++.h>
+
+#define N 11
+#define M 11
+#define K 51
+#define R 151
+
 using namespace std;
 
 int n, m, k, r;
+int a[N], b[M], c[K];
+int dp[R];
+int sum;
+int ans = 0;
 
 int main() {
 
     cin >> n >> m >> k >> r;
 
     for(int i = 1; i <= n; ++i) {
-        
+        cin >> a[i];
     }
+
+    sort(a + 1, a + n + 1);
+
+    for(int i = 1; i <= m; ++i) {
+        cin >> b[i];
+    }
+
+    for(int i = 1; i <= m; ++i) {
+        cin >> c[i];
+    }
+
+    for(int i = 1; i <= m; ++i) {
+        for(int j = r; j >= b[i]; --j) {
+            dp[j] = max(dp[j], dp[j - b[i]] + c[i]);
+        }
+    }
+
+    for(int i = 1; i <= r; ++i) {
+        if(dp[i] >= k) {
+            sum = r - i;
+            break;
+        }
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        if(sum > a[i]) {
+            sum -= a[i];
+            ++ans;
+        } 
+        else {
+            break;
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
---
+++ Luogu_P_7859.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define M 401
+
+using namespace std;
+
+int n, m;
+int x[M], y[M];
+
+int main() {
+
+    cin >> n >> m;
+
+    for(int i = 1; i <= m; ++i) {
+        cin >> x[i] >> y[i];
+    }
+
+    
+
+    return 0;
+}
