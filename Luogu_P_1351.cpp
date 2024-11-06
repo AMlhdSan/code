0d578b18de3b824e6488a718698d8c4a749fcfca
AMlhdSan <amlhdsan@gmail.com>
2024-11-06 19:15:46 +0800

    upd

--- Luogu_P_1351.cpp
+++ Luogu_P_1351.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 #define N 200001
+#define MOD 10007
 
 using namespace std;
 
@@ -8,7 +9,10 @@ int n;
 vector<int> edges[N];
 int w[N];
 int tmpu, tmpv;
-int maxx;
+int maxx = -0x7fffffff;
+int tmp1, tmp2;
+int max1, max2;
+int sum = 0;
 
 void add_edges(int u, int v) {
     edges[u].push_back(v);
@@ -29,8 +33,26 @@ int main() {
     }
 
     for(int i = 1; i <= n; ++i) {
-        
+        tmp1 = 0; tmp2 = 0;
+        max1 = -0x7fffffff; max2 = -0x7fffffff;
+        if(edges[i].size() != 1) {
+            for(int v : edges[i]) {
+                tmp1 += w[v] % MOD;
+                tmp2 += (w[v] % MOD * w[v] % MOD) % MOD;
+                if(w[v] >= max1) {
+                    max2 = max1;
+                    max1 = w[v];
+                }
+                else if(w[v] >= max2) {
+                    max2 = w[v];
+                }
+            }
+        }
+        maxx = max(maxx, max1 * max2);
+        sum = (sum + tmp1 * tmp1 % MOD - tmp2) % MOD;
     }
+
+    cout << maxx << ' ' << sum;
 
     return 0;
 }
