c11e167cdf5b433d88bd2171043d9b3803d28d58
AMlhdSan <amlhdsan@gmail.com>
2024-11-06 14:13:44 +0800

    upd

---
+++ Luogu_P_1037.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string n;
+int k;
+int x[N], y[N];
+
+int main() {
+
+    cin >> n + 1;
+
+    cin >> k;
+
+    for(int i = 1; i <= k; ++i) {
+        cin >> x[i] >> y[i];
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        
+    }
+
+    return 0;
+}
