23936a80d70866cf774f895fe7a85cdc531dddda
AMlhd <amlhdsan@gmail.com>
2024-11-09 15:16:47 +0800

    upd

--- Luogu_P_3870.cpp
+++ Luogu_P_3870.cpp
@@ -3,7 +3,7 @@
 #define N 100001
 #define ls (p << 1)
 #define rs (p << 1 | 1)
-#define mid((l + r) >> 1)
+#define mid ((l + r) >> 1)
 
 using namespace std;
 
@@ -17,24 +17,36 @@ void upds(int p) {
     siz[p] = siz[ls] + siz[rs];
 }
 
+void upd(int p) {
+    tree[p] = tree[ls] + tree[rs];
+}
+
 void pushd(int p) {
-    
+    if(lazy[p] & 1) { 
+        tree[ls] = siz[ls] - tree[ls];
+        tree[rs] = siz[rs] - tree[rs];
+        lazy[ls] ^= 1;
+        lazy[rs] ^= 1;
+    }
+    lazy[p] = 0;
 }
 
 void build(int p, int l, int r) {
     if(l == r) {
+        tree[p] = 0;
         siz[p] = 1;
         return;
     }
     build(ls, l, mid);
     build(rs, mid + 1, r);
     upds(p);
+    upd(p);
 }
 
 void mdf(int p, int l, int r, int ql, int qr) {
     if(ql <= l && r <= qr) {
         tree[p] = siz[p] - tree[p];
-        lazy[p] = (lazy[p] + 1) % 2;
+        lazy[p] ^= 1;
         return;
     }
     pushd(p);
@@ -47,8 +59,22 @@ void mdf(int p, int l, int r, int ql, int qr) {
     upd(p);
 }
 
-int main() {
+int qry(int p, int l, int r, int ql, int qr) {
+    if(ql <= l && r <= qr) {
+        return tree[p];
+    }
+    pushd(p);
+    int sum = 0;
+    if(ql <= mid) {
+        sum += qry(ls, l, mid, ql, qr);
+    }
+    if(qr > mid) {
+        sum += qry(rs, mid + 1, r, ql, qr);
+    }
+    return sum;
+}
 
+int main() {
     cin >> n >> m;
 
     build(1, 1, n);
@@ -56,13 +82,12 @@ int main() {
     while(m--) {
         cin >> c >> a >> b;
         if(c == 0) {
-
+            mdf(1, 1, n, a, b);
         }
         else {
-
+            cout << qry(1, 1, n, a, b) << endl;
         }
     }
-
 
     return 0;
 }
--- Luogu_P_4513.cpp
+++ Luogu_P_4513.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
 
 #define N 500005
+#define ls (p << 1)
+#define rs (p << 1 | 1)
+#define mid ((l + r) >> 1)
 
 using namespace std;
 
 int n, m;
 int a[N];
+int tree[N << 2];
+int maxl[N << 2];
+int maxr[N << 2];
+int maxx[N << 2];
 
 inline int read() {
     int f = 1, x = 0;
@@ -23,6 +30,63 @@ inline int read() {
     return x * f;
 }
 
+void upd(int p) {
+    tree[p] = tree[ls] + tree[rs];
+    maxx[p] = max(max(maxx[ls], maxx[rs]), maxl[rs] + maxr[ls]);
+    maxl[p] = max(maxl[ls], tree[ls] + maxl[rs]);
+    maxr[p] = max(maxr[rs], tree[rs] + maxr[ls]);
+}
+
+void build(int p, int l, int r) {
+    if(l == r) {
+        tree[p] = a[l];
+        maxl[p] = a[l];
+        maxr[p] = a[l];
+        maxx[p] = a[l];
+        return;
+    }
+    build(ls, l, mid);
+    build(rs, mid + 1, r);
+    upd(p);
+}
+
+void mdf(int p, int l, int r, int q, int x) {
+    if(q <= l && q >= r) {
+        tree[p] = x;
+        maxl[p] = x;
+        maxr[p] = x;
+        maxx[p] = x;
+        return;
+    }
+    if(q <= mid) {
+        mdf(ls, l, mid, q, x);
+    }
+    if(q > mid) {
+        mdf(rs, mid + 1, r, q, x);
+    }
+    upd(p);
+}
+
+pair<pair<int, int>, pair<int, int> > qry(int p, int l, int r, int ql, int qr) {
+    if(ql <= l && r <= qr) {
+        return (make_pair(make_pair(tree[p], maxl[p]), make_pair(maxr[p], maxx[p])));
+    }
+    else if(ql > mid) {
+        return qry(ls, l, mid, ql, qr);
+    }
+    else if(qr < mid) {
+        return qry(rs, mid + 1, r, ql, qr);
+    }
+    else {
+        pair<pair<int, int>, pair<int, int> > ll = qry(ls, l, mid, ql, qr);
+        pair<pair<int, int>, pair<int, int> > rr = qry(rs, mid + 1, r, ql, qr);
+        pair<pair<int, int>, pair<int, int> > ans;
+        ans.first.first = ll.first.first + rr.first.first;
+        ans.first.second = max()
+
+    }
+}
+
 int main() {
 
     cin >> n >> m;
@@ -31,7 +95,7 @@ int main() {
         a[i] = read();
     }
 
-    build();
+    build(1, 1, n);
 
     return 0;
 }
