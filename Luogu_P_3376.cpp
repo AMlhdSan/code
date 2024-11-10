db118202a02f07ac2850418e8d101004900447fe
AMlhd <amlhdsan@gmail.com>
2024-11-10 22:58:44 +0800

    upd

--- Luogu_P_3376.cpp
+++ Luogu_P_3376.cpp
@@ -1,80 +1,80 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+#define N 500001
+
+using namespace std;
 int n, m, s, t;
-int u, v;
-long long w, ans;
-long long dis[520010];
-int tot=1;
-int vis[520010];
-int pre[520010];
-int head[520010];
-int flag[2510][2510];
+int u,v;
+long long w;
+long long ans, dis[N];
+int tot = 1;
+int now[N], head[N]; 
 
 struct node {
 	int to, nxt;
 	long long val;
-} e[520010];
+}e[N];
 
-inline void add(int u,int v,long long w) {
-	e[++tot].to=v;
-	e[tot].val=w;
-	e[tot].nxt=head[u];
-	head[u]=tot;
+inline void add(int u, int v, long long w) {
+	e[++tot].to = v;
+	e[tot].val = w;
+	e[tot].nxt = head[u];
+	head[u] = tot;
+	
 	e[++tot].to=u;
 	e[tot].val=0;
 	e[tot].nxt=head[v];
 	head[v]=tot;
 }
 
-inline int bfs() { 
-	for(register int i=1;i<=n;i++) vis[i]=0;
+inline int bfs() {  //在惨量网络中构造分层图 
+	for(register int i=1;i<=n;i++) dis[i]=inf;
 	queue<int> q;
 	q.push(s);
-	vis[s]=1;
-	dis[s]=0x7fffffff;
+	dis[s]=0;
+	now[s]=head[s];
 	while(!q.empty()) {
 		int x=q.front();
 		q.pop();
 		for(register int i=head[x];i;i=e[i].nxt) {
-			if(e[i].val==0) continue; 
 			int v=e[i].to;
-			if(vis[v]==1) continue; 
-			dis[v]=min(dis[x],e[i].val);
-			pre[v]=i;
-			q.push(v);
-			vis[v]=1;
-			if(v==t) return 1; 
+			if(e[i].val>0&&dis[v]==inf) {
+				q.push(v);
+				now[v]=head[v];
+				dis[v]=dis[x]+1;
+				if(v==t) return 1;
+			}
 		}
 	}
 	return 0;
 }
 
-inline void update() { 
-	int x=t;
-	while(x!=s) {
-		int v=pre[x];
-		e[v].val-=dis[t];
-		e[v^1].val+=dis[t];
-		x=e[v^1].to;
+inline int dfs(int x,long long sum) {  //sum是整条增广路对最大流的贡献
+	if(x==t) return sum;
+	long long k,res=0;  //k是当前最小的剩余容量 
+	for(register int i=now[x];i&&sum;i=e[i].nxt) {
+		now[x]=i;  //当前弧优化 
+		int v=e[i].to;
+		if(e[i].val>0&&(dis[v]==dis[x]+1)) {
+			k=dfs(v,min(sum,e[i].val));
+			if(k==0) dis[v]=inf;  //剪枝，去掉增广完毕的点 
+			e[i].val-=k;
+			e[i^1].val+=k;
+			res+=k;  //res表示经过该点的所有流量和（相当于流出的总量） 
+			sum-=k;  //sum表示经过该点的剩余流量 
+		}
 	}
-	ans+=dis[t];
+	return res;
 }
 
 int main() {
 	scanf("%d%d%d%d",&n,&m,&s,&t);
 	for(register int i=1;i<=m;i++) {
 		scanf("%d%d%lld",&u,&v,&w);
-		if(flag[u][v]==0) {
-			add(u,v,w);
-			flag[u][v]=tot;
-		}
-		else {
-			e[flag[u][v]-1].val+=w;
-		}
+		add(u,v,w);
 	}
-	while(bfs()!=0) {
-		update();
+	while(bfs()) {
+		ans+=dfs(s,inf);  //流量守恒（流入=流出） 
 	}
 	printf("%lld",ans);
 	return 0;
