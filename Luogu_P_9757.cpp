dc584f7980df637d3ae486b03766ff775e9f38e8
AMlhd <amlhdsan@gmail.com>
2024-11-16 20:07:29 +0800

    upd

--- Luogu_P_9757.cpp
+++ Luogu_P_9757.cpp
@@ -4,56 +4,65 @@
 
 using namespace std;
 
-int n,q,cnt;
+int n,q,sum;
 int a[N],pos[N];
-int x, y;
+int u, v;
 
 int main() {
 
 	cin >> n >> q;
+
 	for(int i = 1; i <= n; ++i) {
 		cin >> a[i]
 		pos[a[i]] = i;
 		if(i != 1 && a[i - 1] + 1 != a[i]){
-			++cnt;
+			++sum;
 		}
 	}
+
     if(1 + a[n] != a[1])
-        ++cnt;
+        ++sum;
 
 	a[n + 1] = a[1];
     a[0] = a[n];
 
 	while(q--) {
-        
-		cin >> x >> y;
 
-		x = pos[x];
-		y = pos[y];
+		cin >> u >> v;
 
-		if(x>y)swap(x,y);
-		if(1+a[(x==1?n:x-1)]!=a[x])cnt--;
-		if(1+a[x]!=a[x+1]&&x!=y-1)cnt--;
-		if(1+a[y-1]!=a[y])cnt--;
-		if(1 + a[y] != a[y + 1])
-            --cnt; 
+		u = pos[u];
+		v = pos[v];
+
+		if(u > v)
+            swap(u, v);
+		if(1 + a[(u == 1? n : u - 1)] != a[u])
+            --sum;
+		if(1 + a[u] != a[u + 1] && u != v - 1)
+            --sum;
+		if(1 + a[v - 1] != a[v])
+            --sum;
+		if(1 + a[v] != a[v + 1])
+            --sum; 
 		
-		swap(a[x],a[y]);
-		pos[a[x]]=x;
-		pos[a[y]]=y;
-		a[n+1]=a[1];
+		swap(a[u], a[v]);
+		pos[a[u]] = u;
+		pos[a[v]] = v;
+
+		a[n + 1] = a[1];
 		
-		if(1 + a[(x == 1 ? n : x - 1)] != a[x])
-            ++cnt;
-		if(1 + a[x] != a[x + 1] && x != y - 1)
-            ++cnt;
-		if(1 + a[y - 1] != a[y])
-            ++cnt;
-		if(1 + a[y] != a[y + 1])
-            cnt++;
+		if(1 + a[(u == 1 ? n : u - 1)] != a[u])
+            ++sum;
+		if(1 + a[u] != a[u + 1] && u != v - 1)
+            ++sum;
+		if(1 + a[v - 1] != a[v])
+            ++sum;
+		if(1 + a[v] != a[v + 1])
+            sum++;
 		 
-		if(cnt<=1)cout<<"DA\n";
-		else cout<<"NE\n";
+		if(sum <= 1)
+            cout << "DA" << endl;
+		else 
+            cout << "NE" << endl;
 	}
 	return 0;
 }
