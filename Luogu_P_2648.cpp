201af7b33064488313085fd1c7f5ea4f388dbf15
AMlhd <amlhdsan@gmail.com>
2024-11-16 11:14:02 +0800

    upd

--- Luogu_P_2648.cpp
+++ Luogu_P_2648.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 int d, p, c, f;
+int s = 0;
 
 int head[N], nxt[N], to[N], w[N];
 int cnt = 0;
 
 void add(int u, int v, int c) {
     nxt[++cnt] = head[u];
-    head[u] = v;
+    head[u] = cnt;
     w[cnt] = c;
     to[cnt] = v;
 }
@@ -27,7 +28,21 @@ int main() {
     for(int i = 1; i <= p; ++i) {
         int u, v;
         cin >> u >> v;
+        add(u, v, -d);
     }
+
+    for(int i = 1; i <= p; ++i) {
+        int u, v, c;
+        cin >> u >> v >> c;
+        add(u, v, c - d);
+    }
+    for(int i = 1; i <= c; ++i) {
+        add(0, i, -d);
+    }
+
+    spfa();
+
+    for(int i = 1; i <= n; )
 
     return 0;
 }
