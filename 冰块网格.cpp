c9cf237cfbf567c732b9f5d474a69c04f2563634
AMlhd <amlhdsan@gmail.com>
2024-11-26 18:22:33 +0800

    upd

--- 冰块网格.cpp
+++ 冰块网格.cpp
@@ -25,7 +25,7 @@ int main() {
     }
 
     for(int i = 2; i <= n; ++i) {
-        for(int j = 2; j <= n; ++i) {
+        for(int j = 2; j <= n; ++j) {
             dp[i][j] = (dp[i - 1][j] * 2 + dp[i][j - 1]) * 2 / 3;
             maxx = max(maxx, dp[i][j]);
         }
