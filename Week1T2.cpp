a30a3e36ce3f2fb218fc731f27c975e052f7b289
AMlhd <amlhdsan@gmail.com>
2024-12-30 21:58:26 +0800

    upd

--- Week1T2.cpp
+++ Week1T2.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
+
+
+#define int long long
+
 using namespace std;
 
 int n;
+int sum = 0;
+int a, b;
+int xo[100010][50];
 
 inline int read() {
     int x = 0, f = 1;
@@ -18,10 +25,26 @@ inline int read() {
     return x * f;
 }
 
-int main() {
+signed main() {
 
     n = read();
-    
 
+    for(int i = 1; i <= n; ++i) {
+        int x = read();
+        for(int j = 0; j <= 31; ++j) {
+            xo[i][j] = xo[i - 1][j] ^ ((x >> j) & 1);
+        }
+    }
+
+    for(int i = 0; i <= 31; ++i) {
+        a = 0;
+        for(int j = 0; j <= n; ++j) {
+            if(xo[j][i] == 0)
+                ++a;
+        }
+        sum += a * (n - a + 1) * (1 << i);
+    }
+
+    cout << sum << endl;
     return 0;
 }
