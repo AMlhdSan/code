32e408a16b669a2cc2a71b0aeeacbf4b6559ef73
AMlhd <amlhdsan@gmail.com>
2025-01-26 22:10:12 +0800

    upd

---
+++ Luogu_P_2422.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define N 100010
+
+using namespace std;
+
+int n;
+int a[N];
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') {
+        if(ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+int main() {
+
+    n = read();
+    
+
+    return 0;
+}
