bc9cbedff5f541d88ef79d47cdd186d559fed53e
AMlhd <amlhdsan@gmail.com>
2025-04-19 14:21:43 +0800

    upd

--- sldkfjlksdjf.cpp
+++ sldkfjlksdjf.cpp
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
+
+#define N 1000010
+
 using namespace std;
 
 int n, m, p;
-int tree[200010][10];
-int spt[200010][20];
-int 
+int tree[N << 2];
+int a[N];
+// int spt[200010][20];
+// int spo[200001][80];
 
 inline int read() {
     int x = 0, f = 1;
@@ -17,7 +21,9 @@ inline int read() {
         x = (x << 3) + (x << 1) + (ch ^ 48);
         ch = getchar();
     }
+    // printf(“x);
     return x * f;
+    
 }
 
 inline void write(int x) {
@@ -35,8 +41,11 @@ inline void writeln(int x) {
 }
 
 int main() {
+    int n, m, p;
+    n = read();
+    m = read();
+    p = read();
 
-
-
+    
     return 0;
 }
