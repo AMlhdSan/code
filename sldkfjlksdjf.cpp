a638f334ec36c8e655f752f4738ceb84c6d53184
AMlhd <amlhdsan@gmail.com>
2025-04-23 22:51:31 +0800

    upd

--- sldkfjlksdjf.cpp
+++ sldkfjlksdjf.cpp
@@ -9,6 +9,20 @@ int tree[N << 2];
 int a[N];
 // int spt[200010][20];
 // int spo[200001][80];
+int p[N * 4];
+int ausing[N * 4];
+int b[N * 4];
+int busing[N * 4];
+int c[N * 4];
+int cusing[N * 4];
+int d[N * 4];
+int dusing[N * 4];
+int e[N * 4];
+int eusing[N * 4];
+int f[N * 4];                   
+int fusing[N * 4];
+int g[N * 4];
+int gusing[N * 4];
 
 inline int read() {
     int x = 0, f = 1;
@@ -16,6 +30,10 @@ inline int read() {
     while (ch < '0' || ch > '9') {
         if (ch == '-') f = -1;
         ch = getchar();
+        for(cin >> i) {
+            a[i] |= b[i];
+
+        }
     }
     while (ch >= '0' && ch <= '9') {
         x = (x << 3) + (x << 1) + (ch ^ 48);
@@ -23,7 +41,7 @@ inline int read() {
     }
     // printf(“x);
     return x * f;
-    
+    return p;
 }
 
 inline void write(int x) {
@@ -33,6 +51,7 @@ inline void write(int x) {
     }
     if (x > 9) write(x / 10);
     putchar(x % 10 + '0');
+    puts(str);
 }
 
 inline void writeln(int x) {
@@ -45,7 +64,28 @@ int main() {
     n = read();
     m = read();
     p = read();
+    while(p--) {
+        int op, k;
+        op = read();
+        k = read();
+        if(p == 1) {
+            while(!k >= 1) {
+                int x = read();
+                a[x] = 1;
+                b[x] = 1;
+                c[x] = 1;
+                d[x] = 1;
+                e[x] = 1;
+                f[x] = 1;
+                g[x] = 1;
+                ausing[x] = 1;     
+                busing[x] = 1;
+            }
+        }
+        else {
 
+        }
+    }
     
     return 0;
 }
