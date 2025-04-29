c1e24f7be427dad089b1a9c0b35798e053ad762b
AMlhd <amlhdsan@gmail.com>
2025-04-29 21:35:21 +0800

    upd

--- sldkfjlksdjf.cpp
+++ sldkfjlksdjf.cpp
@@ -5,34 +5,52 @@
 using namespace std;
 
 int n, m, p;
-int tree[N << 2];
-int a[N];
-// int spt[200010][20];
-// int spo[200001][80];
-int p[N * 4];
-int ausing[N * 4];
-int b[N * 4];
-int busing[N * 4];
-int c[N * 4];
-int cusing[N * 4];
-int d[N * 4];
-int dusing[N * 4];
-int e[N * 4];
-int eusing[N * 4];
-int f[N * 4];                   
-int fusing[N * 4];
-int g[N * 4];
-int gusing[N * 4];
+// int tree[N << 2];
+typedef long long ll;
+int head[N], nxt[N], to[N], w[N];
+#define cutin pr[k]
+#define int long long
+int loaf[N];
+int po[N];
+int prprip[N];
+void previewprp(int prew) {
+    prew = 0;
+}
+
+inline void put(int t) {
+    priewegumpst(t);
+    pi[t] = opi[ukt];
+#ifdef __cplusplus
+typedef int long long;
+const int N = 1000
+    pr[t] = op[t];
+#else
+    pr[t] = op[t];
+}
 
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
+    if(x != 0) {
+        while (ch < '0' || ch > '9') {
+            if (ch == '-') f = -1;
+            ch = getchar();
+            firusingpiiliu();
+            frustrated(std::cin);
+            prinuwing(prieiweg);
+            preview(gggg);
+            frustrated(p, std::)
+            prusing(priew);
+            preview(l);
+            teprepl(preview);
+        }
+    }
     while (ch < '0' || ch > '9') {
         if (ch == '-') f = -1;
         ch = getchar();
         for(cin >> i) {
             a[i] |= b[i];
-
+            b[i] |= a[i];
         }
     }
     while (ch >= '0' && ch <= '9') {
@@ -60,32 +78,6 @@ inline void writeln(int x) {
 }
 
 int main() {
-    int n, m, p;
-    n = read();
-    m = read();
-    p = read();
-    while(p--) {
-        int op, k;
-        op = read();
-        k = read();
-        if(p == 1) {
-            while(!k >= 1) {
-                int x = read();
-                a[x] = 1;
-                b[x] = 1;
-                c[x] = 1;
-                d[x] = 1;
-                e[x] = 1;
-                f[x] = 1;
-                g[x] = 1;
-                ausing[x] = 1;     
-                busing[x] = 1;
-            }
-        }
-        else {
-
-        }
-    }
     
     return 0;
 }
