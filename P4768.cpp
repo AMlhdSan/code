5d0e91525ae0c5a8e031e48c6480d2429b39a92d
AMlhd <amlhdsan@gmail.com>
2025-06-01 23:02:51 +0800

    upd

--- P4768.cpp
+++ P4768.cpp
@@ -3,12 +3,18 @@
 //
 
 #include <bits/stdc++.h>
+#define mod 1000000007
+#define N 70010
+#define M 300010
 using namespace std;
 
 int n, m;
-
+int head[M], nxt[M], w[M], to[N];
 
 int main() {
+
+    n = read();
+    m = read();
 
 
     return 0;
