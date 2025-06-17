a0bfd42a523bd1eb27c7bca4a4840f876a6c32c3
AMlhd <amlhdsan@gmail.com>
2025-06-17 22:33:07 +0800

    upd

--- Luogu_P_5590.cpp
+++ Luogu_P_5590.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-#define N 3010
+#define N 5000
 #define PII pair<int, int>
 
 using namespace std;
@@ -56,37 +56,62 @@ inline void add_edge2(int u, int v, int c) {
 }
 
 inline void dfs1(int p, int pre) {
+    tag1[p] = 1;
     for(PII au : edges[p]) {
         int v = au.first;
-        int index = au.second;
-        dfs1(v, p);
-        tag1[index] = 1;
+        if(!tag1[v])dfs1(v, p);
     }
 }
 
 inline void dfs2(int p, int pre) {
+    tag2[p] = 1;
     for(PII au : adges[p]) {
         int v = au.first;
-        int index = au.second;
-        dfs2(v, p);
-        tag2[index] = 1;
+        // int index = au.second;
+        // tag2[v] = 1;
+        if(!tag2[v])
+            dfs2(v, p);
     }
 }
 
 int dis[N];
 bool vis[N];
+int tot[N];
 
-inline void spfa() {
+inline bool spfa() {
     queue<int> q;
     memset(dis, 0x3f, sizeof(dis));
+    // memset(vis, 0, sizeof(vis));
     vis[1] = 1;
     dis[1] = 0;
+    q.push(1);
+    while(!q.empty()) {
+        int p = q.front();
+        q.pop();
+        vis[p] = 0;
+        ++tot[p];
+        for(int i = head[p]; i; i = nxt[i]) {
+            int v = to[i];
+            if(dis[v] > dis[p] + w[i]) {
+                dis[v] = dis[p] + w[i];
+                if(!vis[v]) {
+                    vis[v] = 1;
+                    q.push(v);
+                }
+                if(tot[v] > n)
+                    return 0;
+            }
+        }
+    }
+    return 1;
 }
 
 int main() {
 
     n = read();
     m = read();
+
+    // memset(head, -1, sizeof(head));
 
     for(int i = 1; i <= m; ++i) {
         du[i] = read();
@@ -99,13 +124,28 @@ int main() {
     dfs2(n, 0);
 
     for(int i = 1; i <= m; ++i) {
-        if(tag1[i] && tag2[i]) {
+        if(tag1[du[i]] && tag2[du[i]] && tag1[dv[i]] && tag2[dv[i]]) {
             add_edge(du[i], dv[i], 9);
             add_edge(dv[i], du[i], -1);
         }
     }
 
-    spfa();
+    bool flag = spfa();
+
+    if(!flag)
+        writeln(-1);
+    else if(dis[n] == 0x3f3f3f3f || dis[n] == 0) {
+		writeln(-1);
+	}
+    else {
+        write(n), putchar(' '), writeln(m);
+        for(int i = 1; i <= m; ++i) {
+            write(du[i]), putchar(' '), write(dv[i]), putchar(' ');
+            if(!tag1[du[i]] || !tag2[du[i]] || !tag1[dv[i]] || !tag2[dv[i]] ) 
+                writeln(1);
+            else writeln(abs(dis[dv[i]] - dis[du[i]]));
+        }
+    }
 
     return 0;
 }
