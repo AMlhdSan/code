551e57d1d313f01a1dea7b6a40b694ee77189e0c
AMlhd <amlhdsan@gmail.com>
2025-08-05 23:41:24 +0800

    upd

--- Luogu_P_2922.cpp
+++ Luogu_P_2922.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int m, n;
-int cnt1[50010];
+// int cnt1[50010];
 // int inf1[50010];
-int tnxt[50010][2];
-bool exi[500100];
+int tnxt[10010][2];
+int exi[500100];
+int fa[500100];
 int tot = 1;
+int ansup[500100];
+int ansdown[500100];
 
 inline int read() {
     int x = 0, f = 1;
@@ -36,6 +39,26 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline void dfs1(int p, int pre) {
+    // fa[p] = pre;
+    ansup[p] += ansup[pre];
+    for(int i = 0; i <= 1; ++i) {
+        if(tnxt[p][i]) {
+            dfs1(tnxt[p][i], p);
+        }
+    }
+}
+
+inline void dfs2(int p, int pre) {
+    // fa[p] = pre;
+    ansdown[pre] += ansdown[p];
+    for(int i = 0; i <= 1; ++i) {
+        if(exi[tnxt[p][i]]) {
+            dfs2(tnxt[p][i], p);
+        }
+    }
+}
+
 int main() {
 
     m = read();
@@ -52,7 +75,31 @@ int main() {
             }
             u = tnxt[u][c];
         }
-        exi[u] = 1;
+        ++exi[u];
+    }
+    for(int i = 1; i <= tot; ++i) {
+        ansup[i] = exi[i];
+        write(ansup[i]);
+        putchar(' ');
+    }
+
+    dfs1(1, 0);
+    dfs2(1, 0);
+
+    while(n--) {
+        int uu = 1;
+        int b, c, u;
+        b = read();
+        for(int i = 1; i <= b; ++i) {
+            c = read();
+            uu = tnxt[uu][c];
+            if(!uu) {
+                u = uu;
+            }
+        }
+        if(uu)
+            u = uu;
+        writeln(ansup[u] + ansdown[u] - exi[u]);
     }
 
     return 0;
