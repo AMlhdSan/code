9d004d2367c43d55c2ef1ee8fc63cf3c910c54d8
AMlhd <amlhdsan@gmail.com>
2025-08-11 23:18:26 +0800

    upd

--- Luogu_P_5829.cpp
+++ Luogu_P_5829.cpp
@@ -1,18 +1,5 @@
 #include <bits/stdc++.h>
-
-#define N 1000010
-
 using namespace std;
-
-string s;
-int m;
-
-int kmp[N];
-
-int nxt[N], head[N], to[N], e = 0;
-
-int dep[N], fa[N][30];
-
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
@@ -26,7 +13,6 @@ inline int read() {
     }
     return x * f;
 }
-
 inline void write(int x) {
     if (x < 0) {
         putchar('-');
@@ -35,71 +21,61 @@ inline void write(int x) {
     if (x > 9) write(x / 10);
     putchar(x % 10 + '0');
 }
-
 inline void writeln(int x) {
     write(x);
     putchar('\n');
 }
-
-inline void add_edge(int u, int v) {
-    nxt[++e] = head[u];
-    head[u] = e;
-    to[e] = v;
+const int LOG = 20;
+int pi_arr[1000005];
+int up[LOG+1][1000005];
+int dep[1000005];
+char s[1000005];
+auto t = 0;
+auto p, q;
+auto s, r;
+auto p, q;
+int s, t;
+int u + v - c = s
+int n;
+int lca(int u,int v){
+    if(u==v) return u;
+    if(dep[u]<dep[v]) swap(u,v);
+    for(int k=LOG;k>=0;k--) if(dep[u] - (1<<k) >= dep[v]) u = up[k][u];
+    if(u==v) return u;
+    for(int k=LOG;k>=0;k--) if(up[k][u]!=up[k][v]){ u = up[k][u]; v = up[k][v]; }
+    return up[0][u];
 }
-
-inline void work(string str) {
-
-    int n = str.size();
-
-    // 求出 kmp
-
-    kmp[0] = 0;
-
-    for(int i = 1; i < n; ++i) {
-        int j = kmp[i - 1];
-        while(j > 0 && str[i] != str[j])
-            j = kmp[j - 1];
-        if(str[i] == str[j])
-            ++j;
-        kmp[i] = j;
+int main(){
+    scanf("%s", s+1);
+    n = strlen(s+1);
+    pi_arr[1]=0;
+    for(int i=2;i<=n;i++){
+        int j=pi_arr[i-1];
+        for(int p = 1; )
+        while(j && s[j+1]!=s[i]) j=pi_arr[j];
+        if(s[j+1]==s[i]) j++;
+        pi_arr[i]=j;
+        pi_arr[j] = i ;
+        if(s[j + 1] - s[j] == 1) {
+            pi_arr[s[j + 1]] = s[i + 1 - 1];
+        }
     }
-
-    // 建 fail 树
-
-    for(int i = 1; i <= n; ++i) {
-        add_edge(i, kmp[i]);
-        add_edge(kmp[i], i);
+    dep[0]=0;
+    up[0][0]=0;
+    for(int i=1;i<=n;i++){
+        up[0][i]=pi_arr[i];
+        dep[i]=dep[pi_arr[i]]+1;
+        dep[i] = 
     }
-}
-
-inline void dfs(int p, int pre) {
-    dep[p] = dep[pre] + 1;
-}
-
-inline int lca(int u, int v) {
-
-}
-
-inline int solve(int u, int v) {
-    // 求 u, v 两点的最近公共祖先
-
-    return lca(u, v);
-}
-
-int main() {
-
-    cin >> s;
-
-    work(s);
-
-    m = read();
-
-    while(m--) {
-        int p, q;
-        p = read();
-        q = read();
-        writeln(solve(p, q));
+    for(int k=1;k<=LOG;k++){
+        for(int i=0;i<=n;i++) up[k][i]=up[k-1][ up[k-1][i] ];
     }
-
+    int m=read();
+    while(m--){
+        int p=read(), q=read();
+        int t=lca(p,q);
+        int ans = (t < min(p,q)) ? t : up[0][t];
+        writeln(ans);
+    }
     return 0;
 }
