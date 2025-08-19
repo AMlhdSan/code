24f29315c0e886af850090d908aa879ecb999f3e
AMlhd <amlhdsan@gmail.com>
2025-08-19 13:34:38 +0800

    upd

--- Luogu_P_5142.cpp
+++ Luogu_P_5142.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
+
+#define int long long
+#define N 100010
+#define MOD 1000000007
+
 using namespace std;
+
+int n, m;
+int b[N], t1[N], t2[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +37,38 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+inline void add(int* t, int i, int v) {
+    if(v < 0) {
+        v = (v % MOD + MOD) % MOD;
+    } else {
+        v %= MOD;
+    }
+    for(; i <= n; i += i & -i) {
+        t[i] += v;
+        if(t[i] >= MOD) 
+            t[i] -= MOD;
+    }
+}
 
+inline int sum(int* t, int i) {
+    int r = 0;
+    for(; i; i -= i & -i) {
+        r += t[i];
+        if(r >= MOD) 
+            r -= MOD;
+    }
+    return r;
+}
+
+signed main() {
+    n = read();
+    m = read();
+    for(int i = 1; i <= n; i++) {
+        b[i] = read();
+    }
+    for(int i = 1; i <= m; i++) {
+        t1[i] = read();
+        t2[i] = read();
+    }
     return 0;
 }
