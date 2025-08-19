9f2a15276bbb6cc6f93ba007e8e73d24b143feec
AMlhd <amlhdsan@gmail.com>
2025-08-19 20:41:55 +0800

    upd

---
+++ T4.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+    int n = read();
+    string s;
+    cin >> s;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        a[i] = read();
+    }
+    
+    unordered_map<int, int> bal;
+    int add = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(') {
+            bal[a[i]]++;
+        } else {
+            if (bal[a[i]] > 0) {
+                bal[a[i]]--;
+            } else {
+                add++;
+            }
+        }
+    }
+    
+    for (auto& p : bal) {
+        add += p.second;
+    }
+    
+    writeln(add);
+    
+    return 0;
+}
