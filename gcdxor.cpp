c3df4f226aa3611303fd33ca08dc03b12b445e24
AMlhd <amlhdsan@gmail.com>
2025-08-21 18:04:02 +0800

    upd

---
+++ gcdxor.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int i,j,k,l,t,n,m,ans;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+
+}
+
+int main() {
+
+    n = read();
+    for(int i = 1; i <= )
+        fo(j,2,n/i)
+            if(((i*j)^(i*(j-1)))==i)ans++;
+    printf("%d\n",ans);
+
+    return 0;
+}
