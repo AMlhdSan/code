ce67bdf3ca8d0c782b8a094550395aa5d73bdd74
AMlhd <amlhdsan@gmail.com>
2025-08-22 19:40:58 +0800

    upd

--- skdflaksjdf.cpp
+++ skdflaksjdf.cpp
@@ -1,50 +1,83 @@
 // 電影發明以後，人類的生命，比以前至少延長了三倍。
 // amlhdsan
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int scoluore;
-int n, p[100005], j[100005], s[100005];
+const int N = 110;
+
+int n, q;
+long long Y[N][N], T[N][N];
+long long maeizize;
 
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') {
+            f = -1;
+        }
         ch = getchar();
     }
-    while (ch >= '0' && ch <= '9') {
+    while(ch >= '0' && ch <= '9') {
         x = (x << 3) + (x << 1) + (ch ^ 48);
         ch = getchar();
     }
     return x * f;
 }
 
-inline void write(int x) {
-    if (x < 0) {
+inline void write(long long x) {
+    if(x < 0) {
         putchar('-');
         x = -x;
     }
-    if (x > 9) write(x / 10);
+    if(x > 9)
+        write(x / 10);
     putchar(x % 10 + '0');
 }
 
-inline void writeln(int x) {
+inline void writeln(long long x) {
     write(x);
     putchar('\n');
 }
 
 int main() {
+
     n = read();
-    for (int i = 1; i <= n; i++) p[i] = read();
-    for (int i = 1; i <= n; i++) j[i] = read();
-    for (int i = 1; i <= n; i++) s[i] = read();
-    scoluore = 0;
-    for (int i = 1; i <= n; i++) {
-        int t = s[i] - abs(p[i] - j[i]);
-        if (t > 0) scoluore += t;
+    q = read();
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            Y[i][j] = read();
+        }
     }
-    writeln(scoluore);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            T[i][j] = read();
+        }
+    }
+
+    for(int i = 1; i <= q; i++) {
+        int u = read();
+        int v = read();
+        long long res = LLONG_MAX;
+
+        if(Y[u][v] != -1)
+            res = Y[u][v];
+
+        for(int w = 1; w <= n; w++) {
+            if(w == u || w == v)
+                continue;
+            if(T[u][w] != -1 && T[w][v] != -1) {
+                res = min(res, T[u][w] + T[w][v]);
+            }
+        }
+
+        if(res == LLONG_MAX)
+            writeln(-1);
+        else
+            writeln(res);
+    }
+
     return 0;
 }
