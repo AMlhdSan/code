bd97f21e9857a408486c4af3e0f6eb48831eef77
AMlhd <amlhdsan@gmail.com>
2025-08-23 15:44:56 +0800

    upd

--- skdflaksjdf.cpp
+++ skdflaksjdf.cpp
@@ -3,81 +3,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 110;
-
-int n, q;
-long long Y[N][N], T[N][N];
-long long maeizize;
-
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
-    while(ch < '0' || ch > '9') {
-        if(ch == '-') {
-            f = -1;
-        }
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
         ch = getchar();
     }
-    while(ch >= '0' && ch <= '9') {
+    while (ch >= '0' && ch <= '9') {
         x = (x << 3) + (x << 1) + (ch ^ 48);
         ch = getchar();
     }
     return x * f;
 }
 
-inline void write(long long x) {
-    if(x < 0) {
+inline void write(int x) {
+    if (x < 0) {
         putchar('-');
         x = -x;
     }
-    if(x > 9)
-        write(x / 10);
+    if (x > 9) write(x / 10);
     putchar(x % 10 + '0');
 }
 
-inline void writeln(long long x) {
+inline void writeln(int x) {
     write(x);
     putchar('\n');
 }
 
+unordered_map<string,string> mp;
+
 int main() {
-
-    n = read();
-    q = read();
-
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            Y[i][j] = read();
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    if (!(cin >> n)) return 0;
+    string ln;
+    getline(cin, ln);
+    for (int i = 0; i < n; ++i) {
+        string s;
+        getline(cin, s);
+        if (s.size() == 0) { --i; continue; }
+        istringstream iss(s);
+        string lang;
+        iss >> lang;
+        string w;
+        while (iss >> w) {
+            for (char &c : w) c = tolower((unsigned char)c);
+            mp[w] = lang;
         }
     }
-
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            T[i][j] = read();
-        }
-    }
-
-    for(int i = 1; i <= q; i++) {
-        int u = read();
-        int v = read();
-        long long res = LLONG_MAX;
-
-        if(Y[u][v] != -1)
-            res = Y[u][v];
-
-        for(int w = 1; w <= n; w++) {
-            if(w == u || w == v)
-                continue;
-            if(T[u][w] != -1 && T[w][v] != -1) {
-                res = min(res, T[u][w] + T[w][v]);
+    getline(cin, ln);
+    while (getline(cin, ln)) {
+        string cur;
+        string ans;
+        for (char c : ln) {
+            if (isalpha((unsigned char)c) || c == '-' || c == '\'' || c == '\\') {
+                cur.push_back(c);
+            } else {
+                if (!cur.empty()) {
+                    string t = cur;
+                    for (char &cc : t) cc = tolower((unsigned char)cc);
+                    auto it = mp.find(t);
+                    if (it != mp.end()) { ans = it->second; break; }
+                    cur.clear();
+                }
             }
         }
-
-        if(res == LLONG_MAX)
-            writeln(-1);
-        else
-            writeln(res);
+        if (ans.empty() && !cur.empty()) {
+            string t = cur;
+            for (char &cc : t) cc = tolower((unsigned char)cc);
+            auto it = mp.find(t);
+            if (it != mp.end()) ans = it->second;
+        }
+        if (!ans.empty()) cout << ans << '\n';
     }
-
     return 0;
 }
