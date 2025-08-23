1950a38b06e5b88e1807da39d918584fd1d1b0bb
AMlhd <amlhdsan@gmail.com>
2025-08-23 22:09:38 +0800

    upd

--- skdflaksjdf.cpp
+++ skdflaksjdf.cpp
@@ -1,84 +1,99 @@
-// 電影發明以後，人類的生命，比以前至少延長了三倍。
-// amlhdsan
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define rrep(i,r,l) for(int i=r;i>=l;i--)
+#define ll long long
+#define pqu priority_queue
+#define umap unordered_map
+
 inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
+    int x=0,f=1;char ch=getchar();
+    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}
+    return x*f;
 }
 
 inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
+    if(x<0){putchar('-');x=-x;}
+    if(x>9)write(x/10);
+    putchar(x%10+'0');
 }
 
 inline void writeln(int x) {
-    write(x);
-    putchar('\n');
+    write(x);putchar('\n');
 }
 
-unordered_map<string,string> mp;
+const int N=30,M=2e5+10;
+int n,m,k,tot;
+
+struct node {
+    int cnt,tim,id;
+    string nam;
+    int res[N];
+} T[M];
+
+bool operator<(const node &x,const node &y){
+    if(x.cnt!=y.cnt)return x.cnt>y.cnt;
+    if(x.tim!=y.tim)return x.tim<y.tim;
+    return x.id<y.id;
+}
+
+pqu<node> pq;
+umap<string,int> idmap;
+int sub[M][N];
+
+int getid(string nam) {
+    if(!idmap.count(nam)){
+        idmap[nam]=++tot;
+        T[tot].cnt=0;T[tot].tim=0;T[tot].id=tot;T[tot].nam=nam;
+        return tot;
+    }
+    return idmap[nam];
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    if (!(cin >> n)) return 0;
-    string ln;
-    getline(cin, ln);
-    for (int i = 0; i < n; ++i) {
-        string s;
-        getline(cin, s);
-        if (s.size() == 0) { --i; continue; }
-        istringstream iss(s);
-        string lang;
-        iss >> lang;
-        string w;
-        while (iss >> w) {
-            for (char &c : w) c = tolower((unsigned char)c);
-            mp[w] = lang;
+    n=read(),m=read(),k=read();
+    rep(i,1,k){
+        int x=read(),y=read(),z=read();
+        char ch,st;
+        string nam,tmp;
+        cin>>ch>>nam>>st;
+        getline(cin,tmp);
+        bool ok=(st=='A');
+        int id=getid(nam),pid=ch-'A'+1;
+        int now=x*60+y;
+        bool op=(now<240||(now==240&&z==0));
+        if(T[id].res[pid])continue;
+        if(op){
+            if(ok){
+                T[id].tim+=sub[id][pid]*20+now;
+                T[id].cnt++;
+                T[id].res[pid]=1;
+            }else sub[id][pid]++;
+        }else{
+            if(ok)T[id].res[pid]=now;
+            else sub[id][pid]++;
         }
     }
-
-    getline(cin, ln);
-    // getline(cin, ln);
-    while (getline(cin, ln)) {
-        string cur;
-        string ans;
-        for (char c : ln) {
-            if (isalpha((unsigned char)c) || c == '-' || c == '\'' || c == '\\') {
-                cur.push_back(c);
-            } else {
-                if (!cur.empty()) {
-                    string t = cur;
-                    for (char &cc : t) cc = tolower((unsigned char)cc);
-                    auto it = mp.find(t);
-                    if (it != mp.end()) { ans = it->second; break; }
-                    cur.clear();
+    rep(i,1,tot)pq.push(T[i]);
+    while(pq.size()>1){
+        node a=pq.top();pq.pop();
+        cout<<a.nam<<'\n';
+        node b=pq.top();pq.pop();
+        rep(i,1,n){
+            if(a.res[i]>1){
+                a.cnt++;
+                a.tim+=sub[a.id][i]*20+a.res[i];
+                a.res[i]=0;
+                if(a<b){
+                    pq.push(a);
+                    break;
                 }
             }
         }
-        if (ans.empty() && !cur.empty()) {
-            string t = cur;
-            for (char &cc : t) cc = tolower((unsigned char)cc);
-            auto it = mp.find(t);
-            if (it != mp.end()) ans = it->second;
-        }
-        if (!ans.empty()) cout << ans << '\n';
+        pq.push(b);
     }
+    cout<<pq.top().nam<<'\n';
     return 0;
 }
