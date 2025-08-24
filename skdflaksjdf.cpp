8e1cb989b31c373377fec8d7b4fba0bb5771735b
AMlhd <amlhdsan@gmail.com>
2025-08-24 19:58:49 +0800

    upd

--- skdflaksjdf.cpp
+++ skdflaksjdf.cpp
@@ -11,7 +11,20 @@ using namespace std;
 
 int dp[N][N];
 
-inline void 
+namespace fastIO {
+    inline int read() {
+        int x=0,f=1;char ch=getchar();
+        while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+        while(ch>='0'&&ch<='9'){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}
+        return x*f;
+    }
+
+    inline void write(int x) {
+        if(x<0){putchar('-');x=-x;}
+        if(x>9)write(x/10);
+        putchar(x%10+'0');
+    }
+}
 
 inline int read() {
     int x=0,f=1;char ch=getchar();
