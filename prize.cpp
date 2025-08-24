16d3aac59011e87b576cbf64b195012e76091996
AMlhd <amlhdsan@gmail.com>
2025-08-24 21:06:53 +0800

    upd

--- prize.cpp
+++ prize.cpp
@@ -1,11 +1,7 @@
+// 電影發明以後，人類的生命，比以前至少延長了三倍。
+// amlhdsan
 #include <bits/stdc++.h>
-
-#define int long long
-
 using namespace std;
-
-int T;
-int n, m, x, y;
 
 inline int read() {
     int x = 0, f = 1;
@@ -36,16 +32,27 @@ inline void writeln(int x) {
 }
 
 int main() {
-
-    T = read();
-    while(T--) {
-        n = read();
-        x = read();
-        y = read();
-        m = read();
-
-
+    int T = read();
+    while (T--) {
+        int n = read(), x = read(), y = read(), m = read();
+        int half = y / 2;
+        bool ok = false;
+        for (int a = 0; a <= n && !ok; a++) {
+            int left1 = n - a;
+            int sum1 = a * y;
+            if (sum1 > m) break;
+            for (int b = 0; b <= left1 && !ok; b++) {
+                int left2 = left1 - b;
+                long long cur = 1ll * a * y + 1ll * b * x;
+                if (cur > m) continue;
+                long long need = m - cur;
+                if (0 <= need && need <= 1ll * left2 * half) {
+                    ok = true;
+                }
+            }
+        }
+        if (ok) puts("Yes");
+        else puts("No");
     }
-
     return 0;
 }
